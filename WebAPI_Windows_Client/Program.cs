231a45ca68eea2f3af59d52bcd69be6c1713aeb1
sarder kamruzzaman <polash.cse10@gmail.com>
2019-12-04 11:21:27 +0600

    refactoring code

--- WebAPI_Windows_Client/Program.cs
+++ WebAPI_Windows_Client/Program.cs
@@ -16,7 +16,7 @@ namespace WebAPI_Windows_Client
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Home());
         }
     }
 }
